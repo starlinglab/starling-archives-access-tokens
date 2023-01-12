69a49f3d2c4d65380b453dd044c4d3e435aa71b4
YurkoWasHere <yurkowashere@users.noreply.github.com>
2023-01-12 12:15:57 -0500

    updated to description and URI

--- contracts/starling.sol
+++ contracts/starling.sol
@@ -11,7 +11,8 @@ contract starling is ERC721URIStorage {
     using Strings for uint256;
     using Counters for Counters.Counter;
     Counters.Counter  private _tokenIds;
-    mapping(uint256=> string) public tokenIdtoCollection;
+    mapping(uint256=> string) public tokenIdtoArchiveURI;
+    mapping(uint256=> string) public tokenIdtoArchiveDescription;
     mapping(uint256=> string) public tokenIdtoLITkey;
     mapping(uint256=> string) public tokenIdtoAccessCondition;
     mapping(uint256=> address) public tokenIdtoCreator;
@@ -26,7 +27,7 @@ contract starling is ERC721URIStorage {
         '<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
         '<rect width="100%" height="100%" fill="black" />',
         '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"Starling Test Access Token",'</text>',
-        '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Collection: ", getCollection(tokenId),'</text>',
+        '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Archive Description: ", getArchiveDescription(tokenId),'</text>',
         '</svg>'
         );
 
@@ -37,8 +38,11 @@ contract starling is ERC721URIStorage {
             )    
         );
     }
-    function getCollection(uint256 tokenId) public view returns (string memory){
-        return tokenIdtoCollection[tokenId];
+    function getArchiveURI(uint256 tokenId) public view returns (string memory){
+        return tokenIdtoArchiveURI[tokenId];        
+    }
+    function getArchiveDescription(uint256 tokenId) public view returns (string memory){
+        return tokenIdtoArchiveDescription[tokenId];
     }
     function getAccessCondition(uint256 tokenId) public view returns (string memory){
         return tokenIdtoAccessCondition[tokenId];
@@ -49,7 +53,7 @@ contract starling is ERC721URIStorage {
     function getTokenURI(uint256 tokenId) public view returns (string memory) {
         bytes memory dataURI = abi.encodePacked(
             '{',
-                '"name": "Starling Access Token for', tokenIdtoCollection[tokenId] , '",',
+                '"name": "Starling Access Token for ', tokenIdtoArchiveDescription[tokenId] , '",',
                 '"description": "Token # ', tokenId.toString() , '",',
                 '"image_data": "', generateImg(tokenId), '"',
             '}'
@@ -66,19 +70,20 @@ contract starling is ERC721URIStorage {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        tokenIdtoCollection[newItemId] = "uninitialized";
+        tokenIdtoArchiveDescription[newItemId] = "uninitialized";
         tokenIdtoLITkey[newItemId] = "";
         tokenIdtoAccessCondition[newItemId] = "";
         tokenIdtoCreator[newItemId] = msg.sender;
         _setTokenURI(newItemId, getTokenURI(newItemId));
         emit Minted(msg.sender, newItemId);
     }
-    function initializeLIT(uint256 tokenId, string memory CollectionName, string memory LitKey, string memory AccessCondition) public {
+    function initializeLIT(uint256 tokenId, string memory ArchiveDescription, string memory LitKey, string memory AccessCondition, string memory ArchiveURI) public {        
         require(_exists(tokenId), "Token not found");
         require(tokenIdtoCreator[tokenId] == msg.sender, "Only creator can update token");
-        tokenIdtoCollection[tokenId] = CollectionName;
+        tokenIdtoArchiveDescription[tokenId] = ArchiveDescription;
         tokenIdtoLITkey[tokenId] = LitKey;
         tokenIdtoAccessCondition [tokenId] = AccessCondition;
+        tokenIdtoArchiveURI[tokenId] = ArchiveURI;
         _setTokenURI(tokenId, getTokenURI(tokenId));
     }
     function tokenCount() public view returns (uint256) {
