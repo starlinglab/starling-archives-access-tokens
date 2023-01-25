adb12855eb85aa95da40e0c9d4626615780196fb
YurkoWasHere <yurkowashere@users.noreply.github.com>
2023-01-25 09:26:07 -0500

    image uri support tokenIdtoExternal_url support burn token support solebind support

--- contracts/starling.sol
+++ contracts/starling.sol
@@ -16,13 +16,15 @@ contract starling is ERC1155URIStorage {
     mapping(uint256=> string) public tokenIdtoArchiveURI;
     mapping(uint256=> string) public tokenIdtoArchiveDescription;
     mapping(uint256=> string) public tokenIdtoLITkey;
+    mapping(uint256 => string) public tokenIdImageUri;
     mapping(uint256=> string) public tokenIdtoAccessCondition;
     mapping(uint256=> address) public tokenIdtoCreator;
     mapping(uint256 => uint256) public tokenSupply;
+    mapping(uint256 => string) public tokenIdtoExternal_url;
+    string public name = "Starling Test Access Tokens";
 
     constructor() ERC1155(""){
     }
-
 
     function getArchiveURI(uint256 tokenId) public view returns (string memory){
         return tokenIdtoArchiveURI[tokenId];        
@@ -36,6 +38,7 @@ contract starling is ERC1155URIStorage {
     function getLITKey(uint256 tokenId) public view returns (string memory){
         return tokenIdtoLITkey[tokenId];
     }
+    //Minting
     event Minted(address owner, uint256 _tokenId);
     function mintAdditional(uint256 _tokenId,uint256 amount) public {
         require(tokenIdtoCreator[_tokenId] == msg.sender, "Only creator can mint more tokens");
@@ -52,24 +55,44 @@ contract starling is ERC1155URIStorage {
         tokenIdtoLITkey[newItemId] = "";
         tokenIdtoAccessCondition[newItemId] = "";
         tokenIdtoCreator[newItemId] = msg.sender;
+        tokenIdtoExternal_url[newItemId] = "";
+        tokenIdtoExternal_url[newItemId] = tokenIdtoExternal_url[1];
+            
         //_setTokenURI(newItemId, getTokenURI(newItemId));
         emit Minted(msg.sender, newItemId);
     }
-    function initializeLIT(uint256 tokenId, string memory ArchiveDescription, string memory LitKey, string memory AccessCondition, string memory ArchiveURI) public {        
+    function burnToken(address owner, uint256 _tokenId) public {
+        require(tokenIdtoCreator[_tokenId] == msg.sender, "Only creator can burn a tokens");
+        _burn(owner,_tokenId,1);
+        tokenSupply[_tokenId]-=1;
+    }
+    //Sole boundish token
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) internal virtual override {
+        super._beforeTokenTransfer(operator, from, to,ids, amounts, data);
+        for (uint256 i = 0; i < ids.length; i++) {
+            require(tokenIdtoCreator[ids[i]] == address(0) || from == tokenIdtoCreator[ids[i]] || to == tokenIdtoCreator[ids[i]], "Tokens can only flow to/from creator");
+        }
+    }
+    event MetadataUpdate(uint256 _tokenId);
+    function initializeLIT(uint256 tokenId, string memory ArchiveDescription, string memory LitKey, string memory AccessCondition, string memory ArchiveURI, string memory ImageURI) public {
         //require(exists(tokenId), "Token not found");
         require(tokenIdtoCreator[tokenId] == msg.sender, "Only creator can update token");
         tokenIdtoArchiveDescription[tokenId] = ArchiveDescription;
         tokenIdtoLITkey[tokenId] = LitKey;
         tokenIdtoAccessCondition [tokenId] = AccessCondition;
         tokenIdtoArchiveURI[tokenId] = ArchiveURI;
+        tokenIdImageUri[tokenId] = ImageURI;
+        emit MetadataUpdate(tokenId);
+    }
+    function setExternalUrl(uint256 tokenId, string memory url) public {
+        //require(exists(tokenId), "Token not found");
+        require(tokenIdtoCreator[tokenId] == msg.sender, "Only creator can update externalURL");
+        tokenIdtoExternal_url[tokenId]=url;
+        emit MetadataUpdate(tokenId);
     }
     function tokenCount() public view returns (uint256) {
         return _tokenIds.current();
     }
-    function totalSupply(uint256 id) public view returns (uint256) {
-        return tokenSupply[id];
-    }   
-
     // Dynamic URI Stuff
     function generateImg(uint256 tokenId) private view returns  (string memory) {
         bytes memory svg = abi.encodePacked(
@@ -92,13 +115,27 @@ contract starling is ERC1155URIStorage {
         return getTokenURI(tokenId);
     }
     function getTokenURI(uint256 tokenId) public view returns (string memory) {
-        bytes memory dataURI = abi.encodePacked(
-            '{',
-                '"name": "Starling Access Token for ', tokenIdtoArchiveDescription[tokenId] , '",',
-                '"description": "Token # ', tokenId.toString() , '",',
-                '"image_data": "', generateImg(tokenId), '"',
-            '}'
-        );
+        bytes memory dataURI;
+        bytes memory tempEmptyStringTest = bytes(tokenIdImageUri[tokenId]);
+        if (tempEmptyStringTest.length == 0) {        
+            dataURI = abi.encodePacked(
+                '{',
+                    '"name": "Starling Access Token for ', tokenIdtoArchiveDescription[tokenId] , '",',
+                    '"description": "Token # ', tokenId.toString() , '",',
+                    '"image_data": "', generateImg(tokenId), '",',
+                    '"external_url": "', tokenIdtoExternal_url[tokenId], tokenId.toString() ,  '"',
+                '}'
+            );
+        } else {
+            dataURI = abi.encodePacked(
+                '{',
+                    '"name": "Starling Access Token for ', tokenIdtoArchiveDescription[tokenId] , '",',
+                    '"description": "Token # ', tokenId.toString() , '",',
+                    '"image": "',tokenIdImageUri[tokenId], '",',
+                    '"external_url": "', tokenIdtoExternal_url[tokenId], tokenId.toString() ,  '"',
+                '}'
+            );
+        }
         return string(
             abi.encodePacked(
                 "data:application/json;base64,",
